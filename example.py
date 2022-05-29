7c7c99da70ca4dd99fbdf97729d1c84f1d19a0a7
waleadekoya <50258092+waleadekoya@users.noreply.github.com>
2022-05-29 15:39:07 +0100

    added useful links

--- example.py
+++ example.py
@@ -1,28 +1,28 @@
+from pyspark.context import SparkContext
+
+from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.transforms import Join
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
 
 glueContext = GlueContext(SparkContext.getOrCreate())
 
 DATABASE = "legislators"
 OUTPUT_PATH = "s3://glue-python-samples-fbe445ee/output_dir"
 
-## create dynamic frames from data catalog
+# create dynamic frames from data catalog
 persons: DynamicFrame = glueContext.create_dynamic_frame.from_catalog(
     database=DATABASE, table_name="persons_json"
 )
 
 memberships: DynamicFrame = glueContext.create_dynamic_frame.from_catalog(
-    database=DATABASE, table_name="memberships_json"
+    database=DATABASE, tabl_name="memberships_json"
 )
 
 orgs: DynamicFrame = glueContext.create_dynamic_frame.from_catalog(
     database=DATABASE, table_name="organizations_json"
 )
 
-## manipulate data
+# manipulate data
 orgs = (
     orgs.drop_fields(["other_names", "identifiers"])
     .rename_field("id", "org_id")
@@ -36,7 +36,7 @@ l_history = l_history.drop_fields(["person_id", "org_id"])
 
 l_history.printSchema()
 
-## write to s3
+# write to s3
 glueContext.write_dynamic_frame.from_options(
     frame=l_history,
     connection_type="s3",
