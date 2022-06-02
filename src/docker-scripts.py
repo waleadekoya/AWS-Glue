2eddf9ab86696d87f139094d2d74b03a73ff112a
waleadekoya <50258092+waleadekoya@users.noreply.github.com>
2022-06-02 16:29:06 +0100

    glue job setup in-progress

--- src/docker-scripts.py
+++ src/docker-scripts.py
@@ -1,7 +1,9 @@
-import subprocess
 import os
-from glob import glob
+import subprocess
 from pathlib import Path
+
+
+# https://aws.amazon.com/blogs/big-data/develop-and-test-aws-glue-version-3-0-jobs-locally-using-a-docker-container/
 
 
 class AWSGlueLocalDev:
@@ -11,6 +13,8 @@ class AWSGlueLocalDev:
     user_profile = os.environ['USERPROFILE']
     aws_credentials = Path(user_profile, ".aws")
     mount_credentials_dir = f"-v {aws_credentials}:/root/.aws:rw"
+    project_dir = Path(user_profile, "Documents", "AWS-Glue")
+    site_packages = f"-v {project_dir}:/home/glue_user/.local/lib/python3.7/site-packages"
     local_notebook_dir = Path(user_profile, "Documents/notebooks")  # TODO - should be project directory
     powershell = list(Path(os.environ["SYSTEMROOT"], "System32", "WindowsPowerShell").glob("**/powershell.exe"))[0]
     print(powershell)
@@ -24,14 +28,14 @@ class AWSGlueLocalDev:
         self.__container_name = "glue_jupyter"
         self.get_remote_file("https://s3-us-west-2.amazonaws.com/crawler-public/json/serde/json-serde.jar")
         self.create_notebook_dir()
-        # subprocess.run(
-        #     f"{self.pull_docker_image()} "
-        #     f"& {self.remove_container_if_exists()} "
-        #     f"& {self.run_container()} "
-        #     f"& {self.install_extra_packages()}",
-        #     shell=True
-        # )
-        os.system(f"{self.install_extra_packages()}")
+        subprocess.run(
+            f"{self.pull_docker_image()} "
+            f"& {self.remove_container_if_exists()} "
+            f"& {self.run_container()} "
+            f"& {self.install_extra_packages()}",
+            shell=True
+        )
+        # os.system(f"{self.install_extra_packages()}")
         # os.system(f"{self.pull_docker_image()} "
         #           f"& {self.remove_container_if_exists()} "
         #           f"& {self.run_container()} "
@@ -57,28 +61,35 @@ class AWSGlueLocalDev:
             self.local_notebook_dir.mkdir(exist_ok=True)
 
     def pull_docker_image(self):
-        return f"docker pull {self.aws_glue_image_v2}"
+        return f"docker pull {self.aws_glue_image_v1}"
 
     def remove_container_if_exists(self):
         return f"docker rm -f /{self.__container_name}"
 
     def run_container(self):
+        # bin_path = "/home/glue_user/.local/bin:${PATH}"
+        bin_path = "$PATH:/home/glue_user/.local/bin"
         return f"docker run -itd " \
                f"{self.map_host_port} " \
                f"{self.map_notebook_port} " \
                f"{self.mount_credentials_dir} " \
                f"{self.mount_notebooks_dir} " \
-               f"{self.set_container_name()} " \
-               f" {self.aws_glue_image_v2} " \
+               f"--name {self.__container_name} " \
+               f" {self.aws_glue_image_v2} -c export PATH=\"{bin_path}\" " \
+               f"{self.site_packages}" \
                f"{self.start_notebook_server} "
 
     def install_extra_packages(self):
-        packages = "deep-translator pandas==1.4.2"
+        packages = "deep-translator pandas==1.3.5"
         start_container = f"docker container start {self.__container_name}"
+        # https://stackoverflow.com/a/48835704
         return f"{start_container} " \
-               f"& docker exec -it {self.__container_name} /nin/bash " \
-               f"& python -m pip install --no-cache-dir {packages}"
+               f"& docker exec {self.__container_name} /bin/bash " \
+               f"& docker exec {self.__container_name} echo \"$PATH\" " \
+               f"& docker exec {self.__container_name} python3 -m pip install {packages}"
 
 
 if __name__ == "__main__":
+    import sys
+    print(sys.executable)
     AWSGlueLocalDev()
---
+++ src/glue-job.py
@@ -0,0 +1,88 @@
+import sys
+
+from pyspark import SparkContext
+from pyspark.sql.functions import udf, lit
+from pyspark.sql.types import StringType
+from deep_translator import GoogleTranslator
+
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.job import Job
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+
+# https://aws.amazon.com/blogs/big-data/building-python-modules-from-a-wheel-for-spark-etl-workloads-using-aws-glue-2-0/
+# https://aws.amazon.com/premiumsupport/knowledge-center/glue-version2-external-python-libraries/
+
+## @params: [JOB_NAME]
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+
+job.init(args['JOB_NAME'], args)
+
+# =####################### Custom Code Starts Here ####################################=
+
+# Read the data
+datasource0 = glueContext.create_dynamic_frame.from_catalog(database="customers_database", table_name="customers_csv",
+
+                                                            transformation_ctx="datasource0")
+# Convert the DynamicFrame to Spark DataFrame
+spark_df = datasource0.toDF()
+
+
+# Make Transformations
+def translate(col):
+    return GoogleTranslator(source="auto", target="en").translate(col)
+
+
+udf_translate = udf(lambda x: translate(x), StringType())
+spark_df = spark_df.filter((spark_df.title == "Ms.")).limit(5).withColumn("companyname", lit("spital"))
+spark_df = spark_df.withColumn("companyname_en", udf_translate("companyname"))
+spark_df.show()
+
+# Convert backed to a Dynamic DataFrame
+dy_df = DynamicFrame.fromDF(spark_df, glueContext, "dy_df")
+
+# Result are store back to the S3 bucket
+# https://docs.aws.amazon.com/glue/latest/dg/aws-glue-programming-etl-format.html
+dest_bucket = "s3://aws-glue-data-source-az/data/customers_database/outputs/"
+dataset = glueContext.write_dynamic_frame.from_options(frame=dy_df, connection_type="s3",
+                                                       connection_options=dict(path=dest_bucket),
+                                                       format="csv",
+                                                       format_options={
+                                                           "quoteChar": -1,
+                                                           "separator": "|"
+                                                       },
+                                                       transformation_ctx="dataset")
+
+# =####################### Custom Code Ends Here ####################################=
+
+job.commit()
+
+"""
+# BoilerPlate Pyspark Code
+============================
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+job.commit()
+"""
